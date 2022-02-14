6d958d748993a5020875156a960b46a4e6b21e6d
hirotomo <university0120413@gmail.com>
2022-02-14 17:44:28 +0900

    new files3

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World(*'▽')");
+app.get("/", (req, res) => {
+  //res.send("Hello World(*'▽')");
+  res.json({ key: 1 });
 });
 
-//app.listen(3000, function () {});
+app.get("/api/get", (req, res) => {
+  res.send("Hello World(*'▽')/api/getからやで");
+});
+
+/*app.listen(3000, () => {
+  console.log("Application started");
+});
+*/
 process.env.NOW_REGION ? (module.express = app) : app.listen(PORT);
