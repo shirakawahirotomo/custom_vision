028feefe7a5105a8e9e9ae54e3b1810b583ab391
hirotomo <university0120413@gmail.com>
2022-02-15 16:38:20 +0900

    changed index.js of form of json

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; //Heroku用
 
 app.get("/", (req, res) => {
   //res.send("Hello World(*'▽')");
-  res.json({ key: 1 });
+  res.json({ key: 2 });
 });
 
 app.get("/api/get", (req, res) => {
   res.send("Hello World(*'▽')/api/getからやで");
 });
 
+//ローカル用サーバ
 /*app.listen(3000, () => {
   console.log("Application started");
 });
 */
-process.env.NOW_REGION ? (module.express = app) : app.listen(PORT);
+process.env.NOW_REGION ? (module.express = app) : app.listen(PORT); //Heroku用のサーバ樹立
