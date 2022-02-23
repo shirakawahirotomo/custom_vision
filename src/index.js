8c3a96abe7af4f4f35f1718e31e68f8b8fd0bcd7
hirotomo <university0120413@gmail.com>
2022-02-23 23:24:35 +0900

    kadai

--- src/index.js
+++ src/index.js
@@ -1,18 +1,90 @@
+//POSTだけできない
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000; //Heroku用
+//const PORT = process.env.PORT || 3000; //Heroku用
+const bodyParser = require("body-parser");
+//const post = require("v1/post");
 
-/*app.get("/", (req, res) => {
+app.get("/", (req, res) => {
   //res.send("Hello World(*'▽')");
-  res.json({ key: 2 });
+  res.json({ message: "こちらはルートパスです(V)o￥o(V)" });
 });
-*/
-app.get("/api/get", (req, res) => {
+
+app.get("/api/get/", (req, res) => {
   res.send("Hello World(*'▽')/api/getからやで");
 });
 
-//ローカル用サーバ
-/*app.listen(3000, () => {
+//app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+//post.post();
+
+app.post("/api/post", (req, res) => {
+  console.log(req.body);
+  res.send("POSTされたデータを取得できました");
+  //res.status(200);
+});
+
+//ローカル用サーバ/*
+app.listen(3000, () => {
   console.log("Application started");
-});*/
-process.env.NOW_REGION ? (module.express = app) : app.listen(PORT); //Heroku用のサーバ樹立
+});
+
+//process.env.NOW_REGION ? (module.express = app) : app.listen(PORT); //Heroku用
+
+//http://localhost:3000
+
+/*
+//ネットにあったPOST
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.status().json({
+    message: "Hello!",
+  });
+});
+
+app.listen(3000, () => console.log("Listening on port 3000..."));
+*/
+
+/*
+//がいさんのコード
+const express = require("express");
+const post = require("v1/post");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+post.post();
+
+app.get("/", function (req, res) {
+  // res.send('Hello World!');
+
+  // JSON形式でレスポンス返す
+  const resData = {
+    message: "Hello World2",
+  };
+  res.json(resData);
+});
+
+app.get("/api/get", function (req, res) {
+  res.send("api/get: Hello World!");
+});
+
+app.post("/api/post", function (req, res) {
+  // console.log(req.body);
+  const data = req.body;
+  const name = data.Name;
+  console.log("name", name);
+  res.send("api/post: Hello World!");
+});
+
+app.listen(3000, function () {});
+//(process.env.NOW_REGION) ? module.exports = app : app.listen(PORT); // Vercel用
+*/
