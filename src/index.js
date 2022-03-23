e70b480a5a88e3e30dcdfde5f5ee41c5c621624e
bi(cetle) <university0120413@gmail.com>
2022-03-23 13:53:10 +0900

    CETL

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000; //Heroku用
 const bodyParser = require("body-parser");
-//const post = require("v1/post");
-//const request = require("request");
+const request = require("request");
 
 app.get("/", (req, res) => {
   //res.send("Hello World(*'▽')");
@@ -18,13 +17,14 @@ app.use(bodyParser.json()); //必須
 app.use(express.urlencoded({ extended: true }));
 //post.post();
 
-app.post("/api/post", (req, res) => {
+const ID = app.post("/api/post", (req, res) => {
   const data = req.body;
   console.log("req.bodyだよ", data);
   res.send("APIはOkay!!");
   const messageId = data["events"][0]["message"]["id"];
   console.log("画像取得", messageId);
   //res.status(200);
+  return messageId
 });
 
 
@@ -32,7 +32,7 @@ const accessToken =
   "2URYJ4A8RIw4FCltZeYploctm4mVqGAlxEnu340WQV+P93maUNOrOaX6EZRvaHLTAUlsPWMqK7aFb6KW1NHSMcWvZbnOgmTUwh/GE+zu62EiEZJ+Tp+NYnhFHkIlR3GRa1x0OwwtUOFd7J3crIwE4wdB04t89/1O/w1cDnyilFU=";
 
 const options = {
-  url: "https://api-data.line.me/v2/bot/message/messageId/content",
+  url: "https://api-data.line.me/v2/bot/message/ID/content",
   method: "get",
   headers: {
     Authorization: "Bearer " + accessToken,
@@ -40,7 +40,7 @@ const options = {
   encoding: null,
 };
 
-request(options, function (error, response, body) {
+request(options, (error, response, body)=> {
   const buffer = new Buffer.from(body);
   console.log(buffer);
 });
